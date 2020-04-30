24fb0f223fa4324c48dbc14cf825d060ea833e4a
kettle <kettleofrassel@gmail.com>
2020-04-30 12:02:28 -0700

    first minimal version

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld msg="Message" />
+    <Todo />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Todo from "./components/Todo.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Todo
   }
 };
 </script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3>Todo App</h3>
+    <form @submit="addTodo">
+      <input type="text" v-model="text" />
+      <input type="submit" value="Add" />
+    </form>
+    <ul>
+      <li v-for="(todo, inx) in todos" :key="inx">
+        <span :class="todo.completed ? 'completed' : ''">
+          <input type="checkbox" v-model="todo.completed" />
+          {{todo.text}}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  data() {
+    return {
+      text: "",
+      todos: [
+        { text: "washing dishes", completed: false },
+        { text: "jogging", completed: true },
+        { text: "clean the code", completed: false }
+      ]
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      this.todos.unshift({ text: this.text, completed: false });
+      this.text = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
