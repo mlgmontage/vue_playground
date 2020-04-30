7de9e6478f5512b6c5855cdfaec714cfc95cc4ac
kettle <kettleofrassel@gmail.com>
2020-04-30 10:43:30 -0700

    basic list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld msg="Message" />
+    <List />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import List from "./components/List.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    List
   }
 };
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Component</h2>
+    <form @submit="add">
+      <input type="text" name="todo" v-model="todo" />
+    </form>
+    <ol>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo | kittyfy }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      todos: ["kitkat", "bison", "hikki"],
+      todo: ""
+    };
+  },
+  methods: {
+    add(e) {
+      // messy method
+      e.preventDefault();
+      this.todos.push(this.todo);
+      this.todo = "";
+    }
+  },
+  filters: {
+    kittyfy(el) {
+      return el + "fy";
+    }
+  }
+};
+</script>
