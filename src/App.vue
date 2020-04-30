1a3b9bc896cabd16a6029f0f10e7f1dea82bc4bc
kettle <kettleofrassel@gmail.com>
2020-04-30 11:21:03 -0700

    basic form elements

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld msg="Message" />
+    <Forms />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Forms from "./components/Form.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Forms
   }
 };
 </script>
---
+++ src/components/Form.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h2>Vue forms</h2>
+    <form>
+      <label>
+        <input v-model="name" placeholder="type your name" type="text" />
+        {{name}}
+      </label>
+      <br />
+      <label>
+        <input v-model="surname" placeholder="type your surname" type="text" />
+        {{surname}}
+      </label>
+      <br />
+      <label>
+        Are you member?
+        <input type="checkbox" v-model="isMember" />
+        {{ isMember ? 'yes' : 'no'}}
+      </label>
+      <br />
+      <label>
+        enter your favorite controllers
+        <label
+          v-for="(controller, index) in controllers"
+          :key="index"
+        >
+          <input type="checkbox" v-model="selectedControllers" :value="controller" />
+          {{ controller}}
+        </label>
+        {{ selectedControllers }}
+      </label>
+      <label>
+        <select v-model="selectedController" multiple>
+          <option disabled>Please select one</option>
+          <option v-for="(controller, index) in controllers" :key="index">{{ controller}}</option>
+        </select>
+        {{ selectedController }}
+      </label>
+      <label>
+        <label v-for="(editor, inx) in editors" :key="inx">
+          <input v-model="favEditor" type="radio" name="mode" :value="editor" />
+          {{ editor}}
+        </label>
+        {{ favEditor}}
+      </label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "forms",
+  data() {
+    return {
+      name: "",
+      surname: "",
+      isMember: false,
+      selectedControllers: [],
+      selectedController: [],
+      favEditor: "",
+      controllers: [
+        "keyboard",
+        "joystick",
+        "mouse",
+        "arcade stick",
+        "dualshock"
+      ],
+      editors: ["vscode", "atom", "sublime text", "vim", "emacs"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+label {
+  display: block;
+}
+</style>
