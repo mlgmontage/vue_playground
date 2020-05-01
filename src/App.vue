3c6aff22688f30061ac6891795194973fea8ca39
mlgmontage <kettleofrassel1@gmail.com>
2020-05-01 08:52:48 -0700

    first simple grammar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="container">
     <HelloWorld msg="Message" />
+    <tracery />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import tracery from "./components/tracery.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    tracery,
   },
 };
 </script>
---
+++ src/components/tracery.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3 class="text-primary">Tracery</h3>
+    <p class="text-info">{{ story }}</p>
+  </div>
+</template>
+
+<script>
+import traceryGrammar from "tracery-grammar";
+
+const grammar = traceryGrammar.createGrammar({
+  animal: ["panda", "fox", "capybara"],
+  emotion: ["sad", "happy", "happy", "angry"],
+  origin: ["I am #emotion.a# #animal#"],
+});
+
+grammar.addModifiers(traceryGrammar.baseEngModifiers);
+
+export default {
+  name: "tracery",
+  data() {
+    return {
+      stories: [],
+      story: null,
+    };
+  },
+  mounted() {
+    this.story = grammar.flatten("#origin#");
+  },
+};
+</script>
