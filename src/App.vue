a76a70b9a3f8ba3ccd146b97afdaca599423625c
mlgmontage <kettleofrassel1@gmail.com>
2020-05-01 08:41:07 -0700

    fetching questions

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="container">
     <HelloWorld msg="Message" />
+    <Quiz />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Quiz from "./components/Quiz.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Quiz,
   },
 };
 </script>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Quiz</h3>
+    <div v-for="(question, index) in questions" :key="index">
+      <h5 v-html="question.question"></h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz",
+  data() {
+    return {
+      questions: null,
+    };
+  },
+  mounted() {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((response) => response.json())
+      .then((jsonData) => (this.questions = jsonData.results));
+  },
+};
+</script>
